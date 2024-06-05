38994009b99fdd2a5a31407ed867493631eb6df7
Daniele De Marco <ask.ddemarco@gmail.com>
2024-06-05 11:47:10 +0200

    Initial Commit

---
+++ ObsidianCLI/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ObsidianCLI
+//
+//  Created by Daniele De Marco on 05/06/24.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
